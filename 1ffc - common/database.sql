244e4ee88e175d079848dbba6435d549bb91bc6d
asrar_saloda <asrar_saloda@2f766ef0-04b6-4314-9fa0-780eafd91ded>
2023-12-14 13:44:40 +0000

    FFFC-54: Renamed topic name (removed prefix) 	100_bills_documents to bills_documents 	200_payments to payments 	300_system to system 	400_marketing to marketing 	500_collection to collection

--- 1ffc - common/database.sql
+++ 1ffc - common/database.sql
@@ -1,19 +1,19 @@
 -- SQL scripts to populate default data in auth_topic_channel table for contact preferences.
 INSERT INTO auth_topic_channel
 (atc_topic, atc_channel, atc_hardcoded, atc_default, atc_visible)
-VALUES('400_marketing', 'email', NULL, 'N', 'Y');
+VALUES('marketing', 'email', NULL, 'N', 'Y');
 INSERT INTO auth_topic_channel
 (atc_topic, atc_channel, atc_hardcoded, atc_default, atc_visible)
-VALUES('400_marketing', 'postal', NULL, 'N', 'Y');
+VALUES('marketing', 'postal', NULL, 'N', 'Y');
 INSERT INTO auth_topic_channel
 (atc_topic, atc_channel, atc_hardcoded, atc_default, atc_visible)
-VALUES('400_marketing', 'sms', NULL, 'N', 'Y');
+VALUES('marketing', 'sms', NULL, 'N', 'Y');
 INSERT INTO auth_topic_channel
 (atc_topic, atc_channel, atc_hardcoded, atc_default, atc_visible)
-VALUES('500_collections', 'email', 'Y', 'Y', 'Y');
+VALUES('collections', 'email', 'Y', 'Y', 'Y');
 INSERT INTO auth_topic_channel
 (atc_topic, atc_channel, atc_hardcoded, atc_default, atc_visible)
-VALUES('500_collections', 'postal', 'Y', 'Y', 'Y');
+VALUES('collections', 'postal', 'Y', 'Y', 'Y');
 INSERT INTO auth_topic_channel
 (atc_topic, atc_channel, atc_hardcoded, atc_default, atc_visible)
-VALUES('500_collections', 'sms', 'Y', 'Y', 'Y');
+VALUES('collections', 'sms', 'Y', 'Y', 'Y');
