f0941685d4f41ad49bf572f8dc6770e70a039c8a
joshua.gentry <joshua.gentry@2f766ef0-04b6-4314-9fa0-780eafd91ded>
2024-02-23 12:15:36 +0000

    Updated to get the payment URL from epayment.url.

--- 1ffc - document esign service/src/com/sorrisotech/svcs/documentesign/service/GetDocumentEsignUrl.java
+++ 1ffc - document esign service/src/com/sorrisotech/svcs/documentesign/service/GetDocumentEsignUrl.java
@@ -35,16 +35,16 @@ public class GetDocumentEsignUrl extends GetDocumentEsignUrlBase{
 	private static final long serialVersionUID = -1133389163946546889L;
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(GetDocumentEsignUrl.class);
-	
+
 	private static final String THE_CURRENT_AMOUNT_DUE = "the current amount due";
-	
+
 	private static final Properties config = new Properties();
-	
+
 	private static SimpleDateFormat dateFormatter = null;
-	
+
 	static {
 		try (var inputStream = GetDocumentEsignUrl.class.getClassLoader().getResourceAsStream("app-config.properties")) {
-			
+
 			if (inputStream != null) {
 				config.load(inputStream);
 				dateFormatter = new SimpleDateFormat(config.getProperty("pdf.effective.date.format"), Locale.US);
@@ -56,7 +56,7 @@ public class GetDocumentEsignUrl extends GetDocumentEsignUrlBase{
 
 	@Override
 	protected ServiceAPIErrorCode processInternal(IRequestInternal request) {
-		
+
 		final String displayAccount = request.getString(IApiDocumentEsign.GetDocumentEsignUrl.displayAccount);
 		final String internalAccount = request.getString(IApiDocumentEsign.GetDocumentEsignUrl.internalAccount);
 		final String customerId = request.getString(IApiDocumentEsign.GetDocumentEsignUrl.customerId);
@@ -71,19 +71,19 @@ public class GetDocumentEsignUrl extends GetDocumentEsignUrlBase{
     	final String flex2 = request.getString(IApiDocumentEsign.GetDocumentEsignUrl.flex2);
     	final String flex3 = request.getString(IApiDocumentEsign.GetDocumentEsignUrl.flex3);
     	final String flex4 = request.getString(IApiDocumentEsign.GetDocumentEsignUrl.flex4);
-    	
+
     	request.setToResponse();
-    	
+
     	final WalletInfo walletInfo = Dao.getInstance().getWalletInfo(sourceId);
-    	
+
     	if (walletInfo == null) {
     		LOGGER.warn("Wallet info not found for source : {}", sourceId);
     		request.setRequestStatus(ServiceAPIErrorCode.Failure);
 			return ServiceAPIErrorCode.Failure;
     	}
-    	
+
     	final Map<String, String> pdfDetailsMap = new HashMap<>();
-    	
+
     	if (!walletInfo.getSourceType().equals("bank")) {
     		pdfDetailsMap.put("DEBIT_CHECKBOX", "Yes");
     		pdfDetailsMap.put("EFFECTIVE_DATE", dateFormatter.format(new Date()));
@@ -92,7 +92,7 @@ public class GetDocumentEsignUrl extends GetDocumentEsignUrlBase{
     		pdfDetailsMap.put("PAYMENT_AMOUNT_RULE", THE_CURRENT_AMOUNT_DUE);
     		pdfDetailsMap.put("PAYMENT_COUNT_RULE", countRule);
     		pdfDetailsMap.put("NAME_ON_PMT_ACCOUNT", walletInfo.getSourceName());
-    		
+
     		pdfDetailsMap.put("DEBIT_CARD_MASKED", walletInfo.getSourceNum());
     		pdfDetailsMap.put("DEBIT_CARD_EXPIRATION", walletInfo.getSourceExpiry());
 
@@ -102,54 +102,54 @@ public class GetDocumentEsignUrl extends GetDocumentEsignUrlBase{
     		pdfDetailsMap.put("BANK_ACCOUNT_MASKED", "");
 
     	} else {
-    		
+
     		final var sourceDetailsMap = getSourceDetails(sourceId);
-    		
+
     		if (sourceDetailsMap == null) {
     			LOGGER.warn("Unable to finds the source details");
     			request.setRequestStatus(ServiceAPIErrorCode.Failure);
     			return ServiceAPIErrorCode.Failure;
     		}
-    		
+
     		pdfDetailsMap.put("EFFECTIVE_DATE", dateFormatter.format(new Date()));
     		pdfDetailsMap.put("NAME_ON_PMT_ACCOUNT", walletInfo.getSourceName());
     		pdfDetailsMap.put("PAYMENT_AMOUNT_RULE", THE_CURRENT_AMOUNT_DUE);
     		pdfDetailsMap.put("EXTERNAL_ACCT", displayAccount);
     		pdfDetailsMap.put("PAYMENT_COUNT_RULE", countRule);
     		pdfDetailsMap.put("PAYMENT_DATE_RULE", dateRule);
-    		
+
     		pdfDetailsMap.put("DEBIT_CHECKBOX", "Off");
     		pdfDetailsMap.put("DEBIT_CARD_MASKED", "");
     		pdfDetailsMap.put("DEBIT_CARD_EXPIRATION", "");
-    		
+
     		pdfDetailsMap.put("ACH_CHECKBOX", "Yes");
     		pdfDetailsMap.put("BANK_NAME", walletInfo.getSourceName());
     		pdfDetailsMap.put("BANK_ACCOUNT_MASKED", walletInfo.getSourceNum());
     		pdfDetailsMap.put("BANK_ROUTING_NUMBER", sourceDetailsMap.get("routingNumber"));
     	}
-    	
-		
+
+
 		final var party = new Party(fullName, email, phone, null);
-		
+
 		final var accessToken = PDFEsignService.getInstance().getAccessToken();
-		
+
 		if (accessToken == null) {
 			LOGGER.debug("Unable to get access token for esign");
     		request.setRequestStatus(ServiceAPIErrorCode.Failure);
 			return ServiceAPIErrorCode.Failure;
 		}
-		
+
 		final var sessionId = PDFEsignService.getInstance().createNewEsignSession(
-				List.of(party), 
+				List.of(party),
 				accessToken
 		);
-		
+
 		if (sessionId == null) {
 			LOGGER.debug("Unable to create host session id for esign");
     		request.setRequestStatus(ServiceAPIErrorCode.Failure);
 			return ServiceAPIErrorCode.Failure;
 		}
-		
+
 		final Map<String, String> metadata = new HashMap<>();
 		metadata.put("account", customerId);
 		metadata.put("date", new SimpleDateFormat("yyyyMMdd").format(new Date()));
@@ -162,39 +162,39 @@ public class GetDocumentEsignUrl extends GetDocumentEsignUrlBase{
 		metadata.put("flex4", flex4);
 
 		final boolean documentAdded = PDFEsignService.getInstance().addDocumentToSession(
-				pdfDetailsMap, 
-				sessionId, 
+				pdfDetailsMap,
+				sessionId,
 				accessToken,
 				fullName,
 				metadata
 		);
-		
+
 		if (!documentAdded) {
 			LOGGER.debug("Unable to add document to session: {}", sessionId);
     		request.setRequestStatus(ServiceAPIErrorCode.Failure);
 			return ServiceAPIErrorCode.Failure;
 		}
-		
+
 		final var url = PDFEsignService.getInstance().getEsignUrlForSession(
-				sessionId, 
+				sessionId,
 				accessToken,
-				fullName, 
+				fullName,
 				email
 		);
-		
+
 		if (url == null) {
 			LOGGER.debug("Unable to get the esign URL for session: {}", sessionId);
     		request.setRequestStatus(ServiceAPIErrorCode.Failure);
 			return ServiceAPIErrorCode.Failure;
 		}
-		
+
 		request.set(IApiDocumentEsign.GetDocumentEsignUrl.SESSION_ID, sessionId);
 		request.set(IApiDocumentEsign.GetDocumentEsignUrl.URL, url);
 		request.setRequestStatus(ServiceAPIErrorCode.Success);
 		return ServiceAPIErrorCode.Success;
 
 	}
-	
+
 	private Map<String, String> getSourceDetails(String sourceId) {
 		final var restTemplate = new RestTemplate();
 
@@ -206,7 +206,7 @@ public class GetDocumentEsignUrl extends GetDocumentEsignUrlBase{
 
         final var objectMapper = new ObjectMapper();
         String requestBodyJson;
-        
+
         try {
             requestBodyJson = objectMapper.writeValueAsString(requestBodyMap);
         } catch (JsonProcessingException ex) {
@@ -215,32 +215,32 @@ public class GetDocumentEsignUrl extends GetDocumentEsignUrlBase{
         }
 
         HttpEntity<String> requestEntity = new HttpEntity<>(requestBodyJson, headers);
-        
-        final var url = config.getProperty("payment.encryption.baseUrl") + "/detokenize";
-        
+
+        final var url = config.getProperty("epayment.url") + "/detokenize";
+
         try {
 	        ResponseEntity<String> responseEntity = restTemplate.exchange(
-	        		url, 
-	        		HttpMethod.POST, 
-	        		requestEntity, 
+	        		url,
+	        		HttpMethod.POST,
+	        		requestEntity,
 	        		String.class
 	        );
-	
+
 	        if (!responseEntity.getStatusCode().is2xxSuccessful()) {
 	        	LOGGER.error(
-	        			"Error occurred while detokenizing source: {}, responseCode: {}, response: {}", 
-	        			sourceId, 
-	        			responseEntity.getStatusCode(), 
+	        			"Error occurred while detokenizing source: {}, responseCode: {}, response: {}",
+	        			sourceId,
+	        			responseEntity.getStatusCode(),
 	        			responseEntity.getBody()
 	        	);
-	        	
+
 	        	return null;
 	        }
-	        
+
         	final var data = objectMapper.readTree(responseEntity.getBody()).get("data").asText();
-            
+
             if (data == null || data.isBlank()) return null;
-            
+
             return Arrays.asList(data.split(";")).stream()
         			.map(val -> val.split("="))
         			.filter(val -> val.length == 2)
@@ -249,8 +249,8 @@ public class GetDocumentEsignUrl extends GetDocumentEsignUrlBase{
         } catch (Exception ex) {
         	LOGGER.error("Error occurred while detokenizing source: {}, exception: {}", sourceId, ex);
 		}
-        
-        return null; 
+
+        return null;
 	}
 
 }
