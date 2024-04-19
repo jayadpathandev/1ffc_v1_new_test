0b32c385b82779ae27b116974102b65c9f98ce12
james.looney <james.looney@2f766ef0-04b6-4314-9fa0-780eafd91ded>
2024-04-19 13:14:42 +0000

    FFFC-263 Contact Preferences Changes - Create a hidden Payment Notice topic FFFC-264 Contact Preferences Changes - Text Label Changes -UI rework to remove mobile input field and topics column -text label changes -reordering of topics -updating sql script to hide sms and certain topics

--- sql/020_topics.sql
+++ sql/020_topics.sql
@@ -9,7 +9,7 @@ select 'marketing', 'postal', 'N', 'Y'
 where not exists (select 1 from auth_topic_channel where atc_topic = 'marketing' and atc_channel = 'postal');
 
 INSERT INTO auth_topic_channel(atc_topic, atc_channel, atc_default, atc_visible)
-select 'marketing', 'sms', 'N', 'Y'
+select 'marketing', 'sms', 'N', 'N'
 where not exists (select 1 from auth_topic_channel where atc_topic = 'marketing' and atc_channel = 'sms');
 
 INSERT INTO auth_topic_channel(atc_topic, atc_channel, atc_hardcoded, atc_default, atc_visible)
@@ -21,5 +21,23 @@ select 'collections', 'postal', 'Y', 'Y', 'Y'
 where not exists (select 1 from auth_topic_channel where atc_topic = 'collections' and atc_channel = 'postal');
 
 INSERT INTO auth_topic_channel(atc_topic, atc_channel, atc_hardcoded, atc_default, atc_visible)
-select 'collections', 'sms', 'Y', 'Y', 'Y'
+select 'collections', 'sms', 'Y', 'N', 'N'
 where not exists (select 1 from auth_topic_channel where atc_topic = 'collections' and atc_channel = 'sms');
+
+INSERT INTO auth_topic_channel(atc_topic, atc_channel, atc_default, atc_visible)
+select 'account', 'email', 'N', 'Y'
+where not exists (select 1 from auth_topic_channel where atc_topic = 'account' and atc_channel = 'email');
+
+INSERT INTO auth_topic_channel(atc_topic, atc_channel, atc_default, atc_visible)
+select 'account', 'postal', 'N', 'Y'
+where not exists (select 1 from auth_topic_channel where atc_topic = 'account' and atc_channel = 'postal');
+
+INSERT INTO auth_topic_channel(atc_topic, atc_channel, atc_default, atc_visible)
+select 'account', 'sms', 'N', 'N'
+where not exists (select 1 from auth_topic_channel where atc_topic = 'account' and atc_channel = 'sms');
+
+UPDATE auth_topic_channel SET atc_default='N', atc_visible='N' WHERE atc_channel='sms';
+
+UPDATE auth_topic_channel SET atc_default='Y', atc_visible='N' WHERE atc_topic='system' AND atc_channel='email';
+
+UPDATE auth_topic_channel SET atc_default='Y', atc_visible='N' WHERE atc_topic='payment' AND atc_channel='email';
