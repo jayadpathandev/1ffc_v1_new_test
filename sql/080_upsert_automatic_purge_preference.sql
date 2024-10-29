b5ded623107c0104c05028dd2cc9762f7e756363
asrar_saloda <asrar_saloda@2f766ef0-04b6-4314-9fa0-780eafd91ded>
2024-10-29 16:57:26 +0000

    FFFC-661: [SUPT1FFC-225] Recurring Payment does not drop when Debit or ACH payment fails.
    - Updating the SQL (080_upsert_automatic_purge_preference.sql) to set payment.recurring.purge.on.fail=false and  Recurring Payment will not drop when Debit or ACH payment fails.

--- sql/080_upsert_automatic_purge_preference.sql
+++ sql/080_upsert_automatic_purge_preference.sql
@@ -1,8 +1,8 @@
 
 -- This is configuration for payment automatic purge on failed status.
-
+-- 1st Franklin has decided they don't want that right now as they haven't reviewed the compliance issues associated with it. - By John
 INSERT INTO db_config (config_key, config_value)
-VALUES ('payment.recurring.purge.on.fail', 'true')
+VALUES ('payment.recurring.purge.on.fail', 'false')
 ON CONFLICT (config_key)
 DO UPDATE SET config_value = EXCLUDED.config_value;
 
